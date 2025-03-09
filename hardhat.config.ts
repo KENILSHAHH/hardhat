2c096036f669dd92b1eda19ead118a4aa519b6d3
kenilshahh <kenilshah1505@gmail.com>
2025-03-10 04:33:45 +0530

    added initial push

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,16 +3,12 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 import * as dotenv from "dotenv";
 dotenv.config(); 
 
-
-
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545",
-    
     },
-
       amoy: {
         url: "https://rpc-amoy.polygon.technology",
         accounts: [process.env.PRIVATE_KEY || ""],
